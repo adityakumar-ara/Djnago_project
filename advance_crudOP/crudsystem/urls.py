a72a1ee35426b33f85c7ac32d9fbb7bfb7d0ee3f
Aditya Kumar <adityaara7667@gmail.com>
2026-05-09 17:22:44 +0530

    add more

--- advance_crudOP/crudsystem/urls.py
+++ advance_crudOP/crudsystem/urls.py
@@ -2,5 +2,6 @@ from django.urls import path,include
 from .import views
 urlpatterns = [
     path('',views.create_student, name='add_show'),
-    path("show/",views.show_student ,name="show_student")
+    path('show/',views.show_student ,name="show_student"),
+    path('signup/',views.student_signup, name='signup'),
 ]
