2f72ad4e3d5498fb0f7e99dbca1fa12c3b3b8e29
Aditya Kumar <adityaara7667@gmail.com>
2026-05-13 21:39:54 +0530

    UPDATE

--- advance_crudOP/crudsystem/urls.py
+++ advance_crudOP/crudsystem/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include
 from .import views
 urlpatterns = [
-    path('',views.create_student, name='add_show'),
-    path('show/',views.show_student ,name="show_student"),
+    path('create/',views.create_student, name='add_show'),
+    path('',views.show_student ,name="show_student"),
     path('signup/',views.student_signup, name='signup'),
+    path('send-test-email/',views.send_user_email, name='send_user_email'),
 ]
--- advance_crudOP/crudsystem/views.py
+++ advance_crudOP/crudsystem/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
+from django.core.paginator import Paginator
 from .forms import StudentRegistration,BasicSignupForm
 from django.contrib import messages
+from django.db.models import Q # <--- Ye zaroori hai
 from .models import *
 # Create your views here.
 def create_student(request):
@@ -17,7 +19,8 @@ def show_student(request):
     students = Student.objects.all()   
     return render (request, 'show.html', {'students':students})   
 
-from django.shortcuts import render, redirect
+
+
 from django.contrib import messages
 from .forms import BasicSignupForm # Apna naya simple form import karein
 
@@ -33,3 +36,45 @@ def student_signup(request):
         form = BasicSignupForm()
         
     return render(request, 'student_signup.html', {'form': form})
+
+
+from django.core.paginator import Paginator
+from django.db.models import Q
+
+def show_student(request):
+    query = request.GET.get('search_query', '').strip()
+    
+    # 1. QuerySet filter aur order karein (Quotes zaroori hain)
+    if query:
+        student_list = Student.objects.filter(
+            Q(std_name__icontains=query) | 
+            Q(std_roll__icontains=query) | 
+            Q(std_village__icontains=query) |
+            Q(department__name__icontains=query) |
+            Q(course__name__icontains=query)
+        ).distinct().order_by('id') # <--- Yahan 'id' quotes mein hai
+    else:
+        student_list = Student.objects.all().order_by('id') # <--- Yahan bhi
+
+    # 2. Paginator ko QuerySet pass karein
+    paginator = Paginator(student_list, 10) 
+    page_number = request.GET.get('page')
+    students = paginator.get_page(page_number)
+
+    return render(request, 'show.html', {
+        'students': students, 
+        'query': query
+    })
+
+
+from django.core.mail import send_mail
+from django.http import HttpResponse
+
+def send_user_email(request):
+    send_mail(
+        'Subject: Welcome to Our Company',
+        'Hello User, ye aapka message hai.',
+        'adityaara7667@gmail.com',
+        ['funnwithaditya@gemail.com'],
+        fail_silently=False,
+    )
