f2befb69f99f1296fd42fc2a6c1169d915db3a71
Aditya Kumar <adityaara7667@gmail.com>
2026-05-13 21:39:05 +0530

    UPDATE

--- advance_crudOP/advance_crudOP/settings.py
+++ advance_crudOP/advance_crudOP/settings.py
@@ -120,3 +120,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com' # Ya aapki company ka host
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'adityaara7667@gmail.com'
+EMAIL_HOST_PASSWORD = 'rhfc hlje ymly tmfe'
