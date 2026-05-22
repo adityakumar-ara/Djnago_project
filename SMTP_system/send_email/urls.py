1909177db035c5b53a92e1366f4c99ac6f14e026
Aditya Kumar <adityaara7667@gmail.com>
2026-05-22 23:09:04 +0530

    add more

--- SMTP_system/send_email/urls.py
+++ SMTP_system/send_email/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
-from django.urls import path,include
-from .import views
-
+from django.urls import path
+from . import views  # Import your views file
 
 urlpatterns = [
-  
+    # This maps 'http://127.0.0.1:8000/send-test-email/' to your view
+    path('send-test-email/', views.send_test_email, name='send_test_email'),
 ]
-
